5ded5bd91a01ddae12462bff5749317050113552
unknown <briwais@MXL2230QJY.clients.nebula2.washington.edu>
2015-07-29 12:15:59 -0700

    Corrected pkg doc name and formatting.

--- R/poc.R
+++
@@ -1,8 +0,0 @@
-#' POC Utility Functions
-#' 
-#' Hopefully these will make things faster.
-#' Contains some shortcuts and colors.
-#' See especially \code{\link{stored_procedure}}.
-#' 
-#' @docType package
-#' @name pocr
---
+++ R/pocr.R
@@ -0,0 +1,12 @@
+#' POC Utility Functions
+#' 
+#' This package collects a variety of functions and variables constructed
+#' for use by POC staff work with POC data. It includes official POC and
+#' portal colors, POC themes, functions for efficiently querying POC databases,
+#' and more.
+#' 
+#' See especially \code{\link{stored_procedure}}.
+#' 
+#' @docType package
+#' @name pocr
+NULL
