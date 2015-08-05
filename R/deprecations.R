8dba589b30250ac8df534e2893f34871fd7fd2e4
unknown <briwais@MXL2230QJY.clients.nebula2.washington.edu>
2015-08-05 16:37:12 -0700

    Deprecations updated.

--- R/deprecations.R
+++ R/deprecations.R
@@ -34,22 +34,23 @@
 #             'function_to_remove' is no longer supported as of pocr VX.X and
 #             will be removed in future versions of pocr. Please check the 
 #             package documentation for alternative solutions or contact the
-#             package administration for support - use 'maintainer('pocr')' to
-#             get contact details.")
+#             package administrator for support - use 'maintainer('pocr')' to
+#             get contact details.
+#             ")
 #             
 #             function_code...
 # }
 
 ###############################################################################
 
-# trend_plot and context_plot were completely replaced with updated versions,
-# no intermediate steps
+# V1.0: trend_plot and context_plot were completely replaced with updated 
+# versions, no intermediate steps
 
-# trend_plot2, trend_plot3, and trend_plot_state were removed from the
+# v1.0: trend_plot2, trend_plot3, and trend_plot_state were removed from the
 # package, no intermediate steps
 
-# stored_procedure was updated with current sp_cr code, sp_cr was given the
-# old stored procedure code and is being treated as a rename
+# V1.0: stored_procedure was updated with sp_cr code, sp_cr is deprecated below
+# as a rename
 
 # read.sql renamed to read_sql
 read.sql <- function(...) { 
@@ -84,111 +85,18 @@ layOut <- function(...) {
     lay_out(...)
 }
 
-# treat as renamed to stored_procedure
-#' Produces strings for stored procedure calls
-#' 
-#' NEEDS UPDATING
-#' 
-#' Every argument defaults to "0" for all. (Except date which defaults to "2008-01-01".)
-#' Arguments will be coerced to character, so plain sequences are fine.
-#' 
-#' Arguments described above as "matches" will be matched if a partial arg is given.
-#' For example, setting parameter \code{sp = "dis"} will be enough to find the
-#' discharge_exit_over_time stored procedure.
-#' 
-#' Vector arguments will be handled appropriately, so if you want to make a query
-#' for all 39 counties just use \code{county = 1:39}. Character counties will be
-#' converted to numerics, so specifying \code{county = "Kitsap"} or \code{county = c("king, pierce")}
-#' will also work (capitalization is ignored).
-#' 
-#' On TO-DO list: make it smarter about recognizing text office and county groups.
-#'
-#' @param sp (matches) Name of stored procedure (sans any "sp_" prefix or suffix such as "counts").
-#' @param db (matches) Database to use. Defaults to "mysql" (equivalent to "test_annie"), the other option
-#' is "sqlserver" (equivalent to "poc").
-#' @param type (matches) Suffix of stored procedure (e.g. "counts", "perCapita", "MR", or "kin").
-#' @param date Point-in-time or cohort entry/exit date(s) here. (See also \code{\link{seq.Date}}.)
-#' @param age age/age group code, see details.
-#' @param race race/ethnicity, see details.
-#' @param gender 1 for Female, 2 for Male (0 for all).
-#' @param init Initial placement setting, see details.
-#' @param last Last placement setting, see details.
-#' @param reason Removal reason.
-#' @param county County code.
-#' @param custody Custody removed from.
-#' @param office Office code.
-#' @param county_grp County Group Code.
-#' @param siblings Sibling group code 0: All, 1: 2 siblings, 2: 3 siblings, 3: 4 siblings, 4: >4 siblings.
-#'
-#' @export
-#' @examples
-#' \dontrun{storedProcedure("poc1ab", date = seq(as.Date("2006-01-01"),
-#'     as.Date("2009-01-01"), by = "year"), age = 1:3)}
-sp_cr <- function(sp = c("poc1ab", "poc2", "poc3",
-                                    "discharge_exit_over_time",
-                                    "discharge_exit_over_time_mr",
-                                    "PBCP5", "PBCS2", "PBCS3", "PBCW3", "PBCW4"),
-                             db = c("mysql", "sqlserver", "poc", "test_annie"),
-                             type = c("", "counts", "perCapita", "MR", "kin"),
-                             date = "2008-01-01",
-                             age = 0,
-                             race = 0,
-                             gender = 0,
-                             init = 0,
-                             last = 0,
-                             reason = 0,
-                             county = 0,
-                             custody = 0,
-                             office = 0,
-                             county_grp = NA,
-                             siblings = 0) {
+# sp_cr renamed as stored_procedure
+sp_cr <- function(...) {
     warning("
-            'sp_cr' (formerly stored_procedure) is deprecated as of pocr V1.0 
-            and will be removed in future versions of pocr. Use 
-            'stored_procedure' instead.
+            'sp_cr' is deprecated as of pocr V1.0 and will be removed in
+            future versions of pocr. Use 'stored_procedure' instead.
             ")
-    sp <- match.arg(sp)
-    type <- match.arg(type)
-    db <- match.arg(db)
-    if (db == "poc") db <- "sqlserver"
-    if (db == "test_annie") db <- "mysql"
     
-    if (sp %in% c("poc1ab", "poc2", "poc3") & type %nin% c("counts", "perCapita")) {
-        type <- "counts"
-    }
-    if (is.character(county)) {
-        county <- ref_lookup_county[tolower(ref_lookup_county[, 2]) %in% tolower(county), 1]
-    }
-    
-    CALL <- mget(names(formals()), sys.frame(sys.nframe()))
-    callArgs <- unlist(lapply(CALL, FUN= paste0, collapse = ","))
-    argList <- list(poc1ab = c("date","age","race","gender","init","last","reason","county","custody"),
-                    poc2 = c("date","age","race","office"),
-                    poc3 = c("date","age","race","office"),
-                    discharge_exit_over_time = c("date","age","race","gender","init","last","reason","county"),
-                    discharge_exit_over_time_mr = c("date","age","race","gender","init","last","reason","county"),
-                    PBCP5 = c("date","age","race","gender","init","last","reason","county"),
-                    PBCS2 = c("date","age","race","gender","office"),
-                    PBCS3 = c("date","race","office"),
-                    PBCW3 = c("date","age","race","gender","init","last","reason","county"),
-                    PBCW4 = c("date","age","race","gender","init","last","reason","county","siblings")
-    )
-    
-    sqlCall <- argList[[sp]]
-    for (i in 1:length(callArgs)) {
-        sqlCall <- str_replace(sqlCall, pattern = names(callArgs)[i], replacement = callArgs[i])
-    }
-    sqlCall <- paste0("'", sqlCall, "'", collapse = ",")
-    if (db == "mysql") sqlCall <- paste0("(", sqlCall, ");")
-    if (nchar(type) > 0) {
-        sp <- paste(sp, type, sep = "_")
-    }
-    if (db == "mysql") sqlCall <- paste0("call sp_", sp, sqlCall)
-    if (db == "sqlserver") sqlCall <- paste0("exec prod_sp_", sp, " ", sqlCall)
-    return(sqlCall)
+    # arguments passed to updated function
+    stored_procedure(...)
 }
 
-# remove due to lack of use
+# remove - unused
 #' Mode of a vector
 #' 
 #' Found on Stackoverflow.
@@ -207,11 +115,10 @@ Mode <- function(x) {
     ux[which.max(tabulate(match(x, ux)))]
 }
 
-# remove
+# remove - maybe not necessary; should get made a data object if want to keep
 #' Stored Procedure Names
 #' 
 #' sp_names returns the nams of all stored procedures.
-
 sp_names <- c("ia_trends_counts", "ia_trends_rates", "ia_safety",
               "ooh_pit_counts", "ooh_pit_rates", "ooh_flow_entries_counts",
               "ooh_flow_entries_rates", "ooh_flow_exits", "ooh_reentry",
@@ -235,6 +142,13 @@ NULL
 #' should be coverted to \code{Date} classes (default is \code{T}).
 #' @export
 portal_clean <- function(df, select = NULL, date = T) {
+    warning("
+            'portal_clean' is no longer supported as of pocr V1.0 and
+            will be removed in future versions of pocr. You probably want
+            `cr_clean` instead. Contact the package administrator for support 
+            if you cannot resolve your issue - use 'maintainer('pocr')' to
+            get contact details.
+            ")
     names(df) <- make.names(names(df, allow_ = F))
     names(df) <- tolower(names(df))
     names(df) <- str_replace_all(names(df), pattern=" |/|\\(|\\)", replacement = ".")
@@ -254,9 +168,17 @@ portal_clean <- function(df, select = NULL, date = T) {
     return(df)
 }
 
-# REMOVE
+# remove - unused
 #' Sets working directory to \code{"S:/Data Portal"}
 #' @export
 resetwd <- function() {
+    warning("
+            'resetwd' is no longer supported as of pocr V1.0 and
+            will be removed in future versions of pocr. You probably want
+            `cr_clean` instead. Contact the package administrator for support 
+            if you cannot resolve your issue - use 'maintainer('pocr')' to
+            get contact details.
+            ")
+    
     setwd("S:/Data Portal")
 }
