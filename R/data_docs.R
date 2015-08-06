d8f125dcd6766f016b45bef41283b3224e9b9c68
unknown <briwais@MXL2230QJY.clients.nebula2.washington.edu>
2015-08-06 12:19:41 -0700

    Created region lookup data and entries.

--- R/data_docs.R
+++ R/data_docs.R
@@ -26,27 +26,36 @@ NULL
 #' @docType data
 NULL
 
+#' @name quickfacts
 #' @title quickfacts (from US Census Bureau)
 #' @description Data frame characteristics of each county and of the state. 
 #' \code{qf_dict} has the column keys.
 #' @docType data
-#' @name quickfacts
 NULL
 
+#' @name ref_lookup_county
 #' @title ref_lookup_county (from test_annie)
 #' @description Data frame with columns for the county_cd, the text county
 #' and the regional code cd_region. Taken from SQL Server table of the same 
 #' name. I've ordered the entries so that row number corresponds to county_cd 
 #' for the actual counties 1:39.
-#' @format A data frame with three columns, county_cd, county, and region_cd
 #' @docType data
-#' @name ref_lookup_county
 NULL
 
+#' @name ref_lookup_county_region
+#' @title ref_lookup_county_region (from test_annie)
+#' @description Data frame with columns for the county_cd, the text county
+#' and the regional code cd_region. Taken from SQL Server table of the same 
+#' name. I've ordered the entries so that row number corresponds to county_cd 
+#' for the actual counties 1:39. Additionally, has columns for region, the
+#' most useful of which are probably region_6_cd, region_6_tx, and small_fl.
+#' @docType data
+NULL
+
+#' @name ref_lookup_office
 #' @title ref_lookup_office (from test_annie)
 #' @description Data frame with columns cd_office, tx_office, 
 #' cd_office_county_grp, and tx_office_county_grp. Taken from SQL Server table 
 #' of the same name.
 #' @docType data
-#' @name ref_lookup_office
 NULL
