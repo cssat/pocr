fd2e54684b6573459982130cb3f904e47d22df36
oieeri26 <oieeri26@gmail.com>
2015-08-26 10:05:43 -0700

    changed date.type arguments

--- R/cr_clean.R
+++ R/cr_clean.R
@@ -9,53 +9,58 @@
 #' \code{T}).
 #' 
 #' @export
-cr_clean <- function(df, select = NULL, date = T,
-                     date.type = 1, qry.type = "all.unique") {
-    names(df) <- str_replace_all(names(df), pattern="&", replacement = "and")
+cr_clean <- function (df, select = NULL, date = T, date.type = 1, qry.type = "all.unique") 
+{
+    names(df) <- str_replace_all(names(df), pattern = "&", replacement = "and")
     names(df) <- make.names(names(df), allow_ = F)
     names(df) <- tolower(names(df))
-    names(df) <- str_replace_all(names(df), pattern=" |/|\\(|\\)", replacement = ".")
-    names(df) <- str_replace_all(names(df), pattern="/", replacement = ".")
-    names(df) <- str_replace_all(names(df), pattern="\\(", replacement = "")
-    names(df) <- str_replace_all(names(df), pattern="\\)", replacement = "")
-    
+    names(df) <- str_replace_all(names(df), pattern = " |/|\\(|\\)", 
+                                 replacement = ".")
+    names(df) <- str_replace_all(names(df), pattern = "/", replacement = ".")
+    names(df) <- str_replace_all(names(df), pattern = "\\(", 
+                                 replacement = "")
+    names(df) <- str_replace_all(names(df), pattern = "\\)", 
+                                 replacement = "")
     qry.row.keep <- rep(TRUE, nrow(df))
-    date.row.keep <- rep(TRUE, nrow(df))
     
     if (any(str_detect(names(df), "qry\\.type"))) {
         qry.type.col <- names(df)[str_detect(names(df), "qry\\.type")]
         if (qry.type == "all.unique") {
             if (any(df[, qry.type.col] == 2)) {
                 qry.row.keep <- df[, qry.type.col] == 2
-            } else {
+            }
+            else {
                 qry.row.keep <- df[, qry.type.col] == 0
-                if (all(!qry.row.keep)) warning("No 'all' or 'unique' query type found.")
-            } 
-        } else {
+                if (all(!qry.row.keep)) 
+                    warning("No 'all' or 'unique' query type found.")
+            }
+        }
+        else {
             qry.row.keep <- df[, qry.type.col] == qry.type
         }
     }
     
+    
     if ("date.type" %in% names(df)) {
         date.type.col <- names(df)[str_detect(names(df), "date\\.type")]
-        if (any(df[, date.type.col] == 1)) {
-            date.type.col <- df[, date.type.col] == 1
-        } else {
-            date.type.col <- df[, date.type.col] == 2
+        if (date.type == 0) {
+            date.row.keep <- df[, date.type.col] == 0
+        }
+        if (date.type == 1) {
+            date.row.keep <- df[, date.type.col] == 1
+        }
+        if (date.type == 2) {
+            date.row.keep <- df[, date.type.col] == 2
         }
     }
     
     df <- df[qry.row.keep & date.row.keep, ]
-    
     uniques <- sapply(df, function(x) length(unique(x)))
     uniques <- names(uniques)[uniques > 1]
-    if (! is.null(select)) {
+    if (!is.null(select)) {
         select <- make.names(select, allow_ = F)
-        
     }
-    
     df <- df[, unique(c(uniques, select))]
-    
     if (date) {
         to.change <- which(sapply(df, is.POSIXct))
         for (i in to.change) {
