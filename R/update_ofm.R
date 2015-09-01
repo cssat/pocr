85d3647e4dc0407c654e142d602dc16de11fe348
Brian Waismeyer <bwaismeyer@gmail.com>
2015-09-01 11:57:33 -0700

    Text edits to ofm.

--- R/update_ofm.R
+++ R/update_ofm.R
@@ -6,15 +6,14 @@
 #' SQL server database. It then drops and recreates the tables with the cleaned 
 #' data.
 #' 
-#' 
 #' After downloading the data from the source, the data is imported as a list of
 #' dataframes, one for each xlsx file. The columns of each dataframe are given
 #' names. We then filter out irrelevant columns, such as area_id and any gender
 #' total counts. Each data frame needs to be filtered in a unique way.
 #' 
 #'  -The total population counts dataframe (total) must include all counts, 
-#'  except the actuall totals. Raced based totals are kept, for the construction 
-#'  of the dataframe for others ethnic backgrounds.
+#'  except the actual totals. Race based totals are kept, for the construction 
+#'  of the dataframe for other ethnic backgrounds.
 #'  
 #'  -The hispanic population count dataframe (hispanic) must include only the 
 #'  totals.
@@ -22,13 +21,13 @@
 #'  -The non-hispanic population count dataframe (non_hispanic) must 
 #'  include only black and white counts.
 #'  
-#'  Construction of the other dataframe: The count for other ethnic background
-#'  (others) is necessary. To construct the dataframe, out all but the 
+#'  - Construction of the other dataframe: The count for other ethnic background
+#'  (others) is necessary. To construct the dataframe, all but the 
 #'  race-based totals from total are filtered. The counts are derived by 
-#'  observing that
+#'  observing that:
 #'  
-#'  others count = total count - hispanic count - non_hipanic black 
-#'                 - non_hispanic white counts
+#'      others count = total count - hispanic count - non_hispanic black 
+#'                     - non_hispanic white counts
 #' 
 #' The dataframes are not tidy: column headers contain the variables race and
 #' gender. Hence, the dataframes are melted with colvars: county, age and 
@@ -38,16 +37,15 @@
 #' All data frames are filtered row-wise to remove irrelevant age groups and
 #' totals.
 #' 
-#' Data is now tidy at this point, but data formating begins. The counts are 
+#' Data is now tidy at this point, but data formatting begins. The counts are 
 #' rounded and values are replaced by reference codes. Age groups are given in 
-#' an inconsistant manner so the data is grouped to get the count for each
+#' an inconsistent manner so the data is grouped to get the count for each
 #' age group.
 #' 
-#' Once formating is finished, rows corresponding to 2015 are created by using
+#' Once formatting is finished, rows corresponding to 2015 are created by using
 #' data from 2014.
 #' 
 #' NOTE: Once 2015 data is available the code requires some upkeep.
-#' 
 #'
 #' @param poc_connection Need to provide an active RODBC connection to the
 #' POC SQL server. NOTE: It is assumed that the object passed in is meaningful.
@@ -64,7 +62,7 @@ update_ofm <- function(poc_connection = NULL,
                        end_year = 2014) {
     
     #test here for 
-    if(poc_connection == NULL){
+    if(is.null(poc_connection)){
         stop("An active RODBC connection must be specified")
     }
     if(start_year < 2000 | end_year < 2000){
@@ -73,7 +71,6 @@ update_ofm <- function(poc_connection = NULL,
     if(start_year > end_year ){
         stop("Start year cannot be greater than end year")
     }
-    
     
     # create year collection
     years <- as.character(start_year:end_year)
@@ -118,22 +115,22 @@ update_ofm <- function(poc_connection = NULL,
     # we want to bind the dataframes in the list together
     ofm_collection <- lapply(ofm_collection, function(x) do.call(rbind, x))
     
-    #All of the df's have the same sort of columns. We use the symbols
-    #below for column names. A_ID is Area_id. The column names that
-    #use only two letters refer to columns names that contain two variables.
-    #The character at the starting index of these names refers to race 
-    #and the last character in the name refers to sex.
+    # All of the df's have the same sort of columns. We use the symbols
+    # below for column names. A_ID is Area_id. The column names that
+    # use only two letters refer to columns names that contain two variables.
+    # The character at the starting index of these names refers to race 
+    # and the last character in the name refers to sex.
     # So, for example, T_T refers to a column for that gives a count for
-    #Total race and Total gender. They are renamed later
-    #A=Asian | B=Black | W=White | T=Total Race | N=Native |
-    #P=Pacific Islander/Hawian | M=Mixed Race
-  
+    # Total race and Total gender. They are renamed later
+    # A=Asian | B=Black | W=White | T=Total Race | N=Native |
+    # P=Pacific Islander/Hawian | M=Mixed Race
+    
     col_names <- c("county_desc", "A_ID", "age_group", "T_T", "T_M", "T_F",
                    "W_T" , "W_M" , "W_F" , "B_T" , "B_M" , "B_F",
                    "N_T" , "N_M" , "N_F" , "A_T" , "A_M" , "A_F",
                    "P_T" ,"P_M" , "P_F" ,"M_T" , "M_M" , "M_F","Year")
     
-    #set col names
+    # set col names
     ofm_collection <- lapply(ofm_collection, function(x){
         colnames(x) <- col_names
         return(x)
@@ -192,8 +189,8 @@ update_ofm <- function(poc_connection = NULL,
     #Eventually we need to replace the counts with a calculated other count
     ofm_collection[[4]] <- filter(ofm_collection[[1]],
                                   tx_race_census == "T") %>%
-                           mutate(tx_race_census = "Other Ethnicity") %>%
-                           select(-column)
+        mutate(tx_race_census = "Other Ethnicity") %>%
+        select(-column)
     
     #We need to refilter and format each table separately 
     #No longer need totals
@@ -202,24 +199,24 @@ update_ofm <- function(poc_connection = NULL,
                                   tx_race_census != "T") %>%
         select(-column) %>%
         mutate(tx_race_census = ifelse(tx_race_census == "A", "Asian",
-                                ifelse(tx_race_census == "B", 
-                                       "Black/African American",
-                                ifelse(tx_race_census == "W",
-                                       "White/Caucasian",
-                                ifelse(tx_race_census == "N",
-                                       "American Indian/Alaskan Native",
-                                ifelse(tx_race_census == "P",
-                                       "Native Hawaiian/Other Pacific Islander",
-                                ifelse(tx_race_census == "M",
-                                       "Multiracial",
-                                       "Unknown")))))
-                                )
-               )
-           
+                                       ifelse(tx_race_census == "B", 
+                                              "Black/African American",
+                                              ifelse(tx_race_census == "W",
+                                                     "White/Caucasian",
+                                                     ifelse(tx_race_census == "N",
+                                                            "American Indian/Alaskan Native",
+                                                            ifelse(tx_race_census == "P",
+                                                                   "Native Hawaiian/Other Pacific Islander",
+                                                                   ifelse(tx_race_census == "M",
+                                                                          "Multiracial",
+                                                                          "Unknown")))))
+        )
+        )
+    
     #Change the race from total to hispanic
     ofm_collection[[2]] <- mutate(ofm_collection[[2]],
                                   tx_race_census = "Hispanic or Latino") %>%
-                           select(-column)
+        select(-column)
     
     #We need to differentiate between non-hisp 
     #black/white and totals df black/white
@@ -228,8 +225,8 @@ update_ofm <- function(poc_connection = NULL,
                tx_race_census == "B" | tx_race_census == "W") %>%
         mutate(tx_race_census =
                    ifelse(tx_race_census == "B",
-                         "Non-Hispanic, Black Alone",
-                         "Non-Hispanic, White Alone")) %>%
+                          "Non-Hispanic, Black Alone",
+                          "Non-Hispanic, White Alone")) %>%
         select(-column)
     
     #round populations as is done in the the excel files
@@ -241,16 +238,16 @@ update_ofm <- function(poc_connection = NULL,
     
     #Join on this to find a common column with age groupings
     ref_replace_age <- data.frame(c( "All", "0-4", "5-9",
-                                    "10-14", "15", "16", "17"), 
-                                 c("All (0-17)",
-                                   "Early Childhood (0 through 4)",
-                                   "Elementary School Age (5 through 9)",
-                                   "Pre to Early Adolescence (10 through 14)",
-                                   "Late Adolescence (15 through 17)",
-                                   "Late Adolescence (15 through 17)",
-                                   "Late Adolescence (15 through 17)")
-                                ) 
-                     
+                                     "10-14", "15", "16", "17"), 
+                                  c("All (0-17)",
+                                    "Early Childhood (0 through 4)",
+                                    "Elementary School Age (5 through 9)",
+                                    "Pre to Early Adolescence (10 through 14)",
+                                    "Late Adolescence (15 through 17)",
+                                    "Late Adolescence (15 through 17)",
+                                    "Late Adolescence (15 through 17)")
+    ) 
+    
     colnames(ref_replace_age) <- c("age_group", "age_grouping")
     
     #Access the sql server to get reference tables 
@@ -261,7 +258,7 @@ update_ofm <- function(poc_connection = NULL,
     ref_lookup_race <- sqlQuery(poc_connection,
                                 "select *
                                  from dbo.ref_lookup_ethnicity_census")
-        
+    
     ref_lookup_age <- sqlQuery(poc_connection,
                                "select  age_grouping_cd, age_grouping
                                 from dbo.ref_age_groupings_census")
@@ -272,9 +269,9 @@ update_ofm <- function(poc_connection = NULL,
     ref_lookup_age$age_grouping <- trimws(ref_lookup_age$age_grouping)
     
     ref_lookup_gender <- sqlQuery(poc_connection,
-                               "select pk_gndr, cd_gndr  
+                                  "select pk_gndr, cd_gndr  
                                 from dbo.ref_lookup_gender")
- 
+    
     #join on county, race, age, sex
     ofm_collection <- lapply(ofm_collection, function(x){
         x <- left_join(x, ref_lookup_county, by = "county_desc")
@@ -283,8 +280,8 @@ update_ofm <- function(poc_connection = NULL,
         x <- left_join(x, ref_lookup_age, by = "age_grouping")
         x <- left_join(x, ref_lookup_gender, by = "cd_gndr")
         x <- select(x, -county_desc, -tx_race_census, 
-                   -tx_race_census, -age_group,
-                   -age_grouping, -cd_gndr)
+                    -tx_race_census, -age_group,
+                    -age_grouping, -cd_gndr)
         return(x)
     }
     )
@@ -323,8 +320,8 @@ update_ofm <- function(poc_connection = NULL,
         left_join(ofm_collection[[4]],
                   tempB, 
                   by = c("source_census", "county_cd", 
-                  "pk_gndr", "age_grouping_cd", 
-                  "Year"))%>%
+                         "pk_gndr", "age_grouping_cd", 
+                         "Year"))%>%
         left_join(tempW, 
                   by = c("source_census", "county_cd",
                          "pk_gndr", "age_grouping_cd",
@@ -372,8 +369,8 @@ update_ofm <- function(poc_connection = NULL,
     colnames(ofm_collection[[5]]) <- c("source_census", "county_cd",
                                        "pk_gndr", "cd_race", "age_grouping_cd",
                                        "measurement_year", "pop_cnt")
-  
-
+    
+    
     
     #sends the df to sql
     #sqlDrop(channel = ca_ods, sqtable = "public_data.mb_census_population")
